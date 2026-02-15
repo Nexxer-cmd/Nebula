78b591dae9abfafb2315d40a0f26f9e49b7faff9
Nexxer-cmd <oarjun790@gmail.com>
2026-02-15 10:10:35 +0530

    Changes in server part

--- nebula-chat-backend/server.js
+++ nebula-chat-backend/server.js
@@ -1277,7 +1277,7 @@ const server = http.createServer(app);
 app.set("trust proxy", 1);
 
 // --- URL CONFIGURATION ---
-const PROD_FRONTEND = "https://nebulachatapp.onrender.com";
+const PROD_FRONTEND = "";
 const CLIENT_URL =
   process.env.NODE_ENV === "production"
     ? PROD_FRONTEND
