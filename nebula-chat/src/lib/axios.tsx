ccbaea3bfc0139d0d841f65e70ab8c3c35aafe2c
Nexxer-cmd <oarjun790@gmail.com>
2026-02-15 10:21:03 +0530

    changes in axios paret

--- nebula-chat/src/lib/axios.tsx
+++ nebula-chat/src/lib/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5000" 
-  : "https://nebulachat-o89w.onrender.com";
+  : "https://nebula-v305.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
